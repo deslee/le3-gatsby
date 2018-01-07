eba0c52f2b593d63ab534b2cc1fb3e74800557f5
Desmond Lee <desmondclee@gmail.com>
2018-01-06 19:54:16 -0600

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,6 @@ import { unstyledLink } from '../utils/reusableStyles';
 
 export const Footer = ({ innerCss }) => (
     <footer css={{...innerCss}}>
-        <p css={{marginTop: 0, marginBottom: '.5rem'}}><a css={{...unstyledLink}} href="https://github.com/deslee/le3.io" target="_blank" rel="noopener">Built using Gatsby</a></p>
+        <p css={{marginTop: 0, marginBottom: '.5rem'}}><a css={{...unstyledLink}} href="https://github.com/deslee/le3-gatsby" target="_blank" rel="noopener">Built using Gatsby</a></p>
     </footer>
 )
